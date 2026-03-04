a713a4022fb44ddf240db559040a024c39ed4b7a
sama-css <orlandomenjivar44@gmail.com>
2026-03-03 21:23:47 -0600

    Se agrega el ejercicio de python semana 6

---
+++ Semana6/Suma.py
@@ -0,0 +1,61 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Sama"
+print(nombre)
+
+edad = 18
+
+print(nombre, edad, "years old")  ## Concatenar
+# bnario = 0
+# binario = 1
+
+
+# string = letras
+# objeto = 012345
+
+# inmutabilidad :
+# pero siempre deben de conservar su tipo
+
+## direccion # 0x
+## tipo=> numerico o string (booleano,objeto)
+## tipo numero=> prepara a la memoria
+
+
+## para guardar un tipo de dato string exa.
+
+## textos largos vamos a utilizar la comillas triples
+
+texto_largo = """kfkhfjefjef,
+dwnjdwnjdw.
+jdbkeqbfkfebkbejjke,
+efinefinekfjnke
+,jenjfenenjeej """
+print(texto_largo)
+## Las comillas triples se usan para textos largos
+# mantienen el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 - numero2
+print("El resultado de la resta es:", resultado)
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 * numero2
+print("El resultado de la multiplicacion es:", resultado)
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 / numero2
+print("El resultado de la division es:", resultado)
