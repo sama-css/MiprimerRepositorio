57e0ccc9d9732d0d31cc8417c6507b71224f9f77
sama-css <orlandomenjivar44@gmail.com>
2026-04-07 22:22:10 -0600

    Laboratorio 02 Fundamentos de programación, ejercicio 2 Crear una función que reciba una palabra y un número, y muestre el resultado en pantalla aplicando la transformación correspondiente.

---
+++ Laboratorio2_Fundamentos/Ejercicio2.py
@@ -0,0 +1,7 @@
+def transformar_palabra(palabra, opcion):
+    if opcion == 1:
+        print(palabra.upper())
+    elif opcion == 2:
+        print(palabra.lower())
+    elif opcion == 3:
+        print(palabra.capitalize())
