2b881cd4334e24584387702267e135453aeaafc9
sama-css <orlandomenjivar44@gmail.com>
2026-04-07 22:14:00 -0600

    Laboratorio 02 Fundamentos de programación, ejercicio 1 Crear una función que reciba un texto y un número.

---
+++ Laboratorio2_Fundamentos/Ejercicio1.py
@@ -0,0 +1,7 @@
+def transformar_texto(texto, opcion):
+    if opcion == 1:
+        return texto.upper()
+    elif opcion == 2:
+        return texto.lower()
+    elif opcion == 3:
+        return texto.capitalize()
