b3b4eb46b8b1788303c894be8eaa6730f8d14d0d
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 21:08:35 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 11 se limpian los espacios en blanco de la cadena de texto, se convierte la primera letra de cada palabra en mayuscula y se centra con un espacio total de 30 caracteres rellenando los espacios con guiones medios

---
+++ Parcial_02_2026_Fundamentos/Ejercicio11.py
@@ -0,0 +1,8 @@
+Toma_la_cadena = "  el nido matinal  "
+limpiar_espacios = Toma_la_cadena.strip()
+primera_mayuscula = limpiar_espacios.title()
+centrar_texto = primera_mayuscula.center(30, "-")
+
+print(Toma_la_cadena)
+print(primera_mayuscula)
+print(centrar_texto)
