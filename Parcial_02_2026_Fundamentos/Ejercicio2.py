0f6df05bffad4622213d9f4ecfd26afa24e0d2ee
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 20:32:30 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 2 se utiliza title() para convertir la primera letra de cada palabra en mayúscula y replace()para reemplazar apellido por nombre

---
+++ Parcial_02_2026_Fundamentos/Ejercicio2.py
@@ -0,0 +1,9 @@
+cadena_nombre = "orlando menjivar"
+
+primera_mayúscula = cadena_nombre.title()
+reemplazar_nombre = cadena_nombre.replace("orlando", "menjivar")
+
+
+print(cadena_nombre)
+print(primera_mayúscula)
+print(reemplazar_nombre)
