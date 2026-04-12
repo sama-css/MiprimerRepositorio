2abd9340869b150f2506e6716e68dc10c80f4712
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 20:49:31 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 6 se utiliza casefold() para una comparacion ignorando mayusculas y se verifica si el texto esta compuesto unicamente por letras con isalpha()

---
+++ Parcial_02_2026_Fundamentos/Ejercicio6.py
@@ -0,0 +1,7 @@
+nombre = "OrlandoSamaelMenjivarBonilla"
+normalizacion_fuerte = nombre.casefold()
+validacion = normalizacion_fuerte.isalpha()
+
+print(nombre)
+print(normalizacion_fuerte)
+print(validacion)
