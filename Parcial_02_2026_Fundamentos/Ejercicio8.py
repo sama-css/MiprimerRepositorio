6f6c6ae4b5f85a8428b633d9c4470258b2ec74de
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 21:02:28 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 8 se define un bloque d texto de 3 lineas utilizando comillas triples, se busca cuanta veces aparece la letra a con find() y se crea una lista de oraciones independientes dividiendo el bloque de texto con splitlines()

---
+++ Parcial_02_2026_Fundamentos/Ejercicio8.py
@@ -0,0 +1,9 @@
+bloque_texto = """Es porque un pajarito de la montana ha hecho
+en el hueco de un arbol, su nido matinal,
+que el arbol amanece con musica en el pecho"""
+contar_a = bloque_texto.count("a")
+dividir_bloque = bloque_texto.splitlines()
+
+print(bloque_texto)
+print("La letra a aparece:", contar_a, "veces")
+print(dividir_bloque)
