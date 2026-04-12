1bc1d622feac7b6061276be4facc087a6a5456f4
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 20:54:35 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 7 se rellena con ceros a la izquierda del texto con el uso de zfill() y se comprueba que el texto final termine con 2 con endswitch()

---
+++ Parcial_02_2026_Fundamentos/Ejercicio7.py
@@ -0,0 +1,7 @@
+texto_numerico = "42"
+rellenar_con_ceros = texto_numerico.zfill(5)
+verificacion = rellenar_con_ceros.endswith("2")
+
+print(texto_numerico)
+print(rellenar_con_ceros)
+print(verificacion)
