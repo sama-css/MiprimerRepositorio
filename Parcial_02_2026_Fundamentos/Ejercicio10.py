231b8a5802d23b1337a943c5b8c21cda14f8520d
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 21:05:50 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 10 se verifica si el texto es alfanumerico y si lo es se  convierte el texto a minúsculas y luego separa la palabra de los números reemplazando 2026 por una cadena vacia

---
+++ Parcial_02_2026_Fundamentos/Ejercicio10.py
@@ -0,0 +1,9 @@
+cadena = "Python2026"
+
+if cadena.isalnum():
+    todo_minusculas = cadena.casefold()
+    reemplazar_2026 = todo_minusculas.replace("2026", "")
+
+print(cadena)
+print(todo_minusculas)
+print(reemplazar_2026)
