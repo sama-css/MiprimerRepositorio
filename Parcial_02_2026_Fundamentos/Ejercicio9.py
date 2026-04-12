7ddbdb2a8d5dfef30e51ad4017e29d9268486fa1
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 21:04:09 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 9 se reemplaza la palabra Any time por Always con replace() y el resultante se convierte en mayusculas con upper()

---
+++ Parcial_02_2026_Fundamentos/Ejercicio9.py
@@ -0,0 +1,7 @@
+cadena = "Any time y Anytime"
+reemplazar_frase = cadena.replace("Any time", "Always")
+todo_mayusculas = reemplazar_frase.upper()
+
+print(cadena)
+print(reemplazar_frase)
+print(todo_mayusculas)
