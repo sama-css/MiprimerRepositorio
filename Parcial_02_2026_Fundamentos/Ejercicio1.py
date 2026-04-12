bcf8ae02bb0c1eab0fa0c6214297d1ba2de2240c
sama-css <orlandomenjivar44@gmail.com>
2026-04-11 20:23:19 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 1 se eliminan espacios en blanco y se cuenta cuántas veces se repit la letra e en una cadena elefante

---
+++ Parcial_02_2026_Fundamentos/Ejercicio1.py
@@ -0,0 +1,10 @@
+cadena_elefante = """    And when I'm back in Chicago, I feel it
+Another version of me, I was in it
+I wave goodbye to the end of beginning    """
+
+sin_espeacios = cadena_elefante.strip()
+cuenta_caracter = sin_espeacios.count("e")
+
+print(cadena_elefante)
+print(sin_espeacios)
+print("La letra e aparece:", cuenta_caracter, "veces")
